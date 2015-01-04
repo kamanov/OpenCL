a970ab70cf299d1c2d1bf3d09919a924ec2f4de4
karama <karimamanov@gmail.com>
2015-01-04 18:53:48 +0300

    fix bug for big int numbers

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -8,9 +8,9 @@
 #include <iomanip>
 #include <iterator>
 #include <cmath>
+#include <string>
+#include <sstream>
 
-typedef float element_type;
-typedef std::vector<element_type> array;
 static const size_t block_size = 256;
 
 struct GpuContext {
@@ -24,36 +24,41 @@ struct GpuContext {
     {}
 };
 
+
 size_t roundToBlockSize(size_t val) {
     return std::ceil((double)val / block_size);
 }
 
-void scan(GpuContext& gpuContext, cl::Buffer& in, cl::Buffer& out, size_t input_size) {
+template<typename T>
+void scan(GpuContext& gpuContext, cl::Buffer& in, cl::Buffer& out, size_t input_size, const std::string& type_name) {
     size_t blocks_num = roundToBlockSize(input_size);
     // allocate auxiliary buffer
-    cl::Buffer dev_aux(gpuContext.context, CL_MEM_READ_WRITE,  sizeof(element_type) * blocks_num);
+    cl::Buffer dev_aux(gpuContext.context, CL_MEM_READ_WRITE,  sizeof(T) * blocks_num);
     size_t rounded_size = blocks_num * block_size;
     // load named kernel from opencl source
-    cl::Kernel kernel_b(gpuContext.program, "scan_blelloch");
+    std::string scan_name = std::string("scan_blelloch_") + type_name;
+    cl::Kernel kernel_b(gpuContext.program, scan_name.c_str());
     cl::KernelFunctor scan_b(kernel_b, gpuContext.queue, cl::NullRange, cl::NDRange(rounded_size), cl::NDRange(block_size));
-    cl::Event event = scan_b(in, out, dev_aux, cl::__local(sizeof(element_type) * block_size), input_size);
+    cl::Event event = scan_b(in, out, dev_aux, cl::__local(sizeof(T) * block_size), input_size);
     event.wait();
     
     if (blocks_num == 1) return;
    
+    cl::Buffer dev_scan_aux(gpuContext.context, CL_MEM_READ_WRITE,  sizeof(T) * blocks_num);
     // scan aux buffer
-    scan(gpuContext, dev_aux, dev_aux, blocks_num);
+    scan<T>(gpuContext, dev_aux, dev_scan_aux, blocks_num, type_name);
     
     // add aux buffer to out
-    cl::Kernel kernel_add(gpuContext.program, "add_aux");
+    std::string add_name = std::string("add_aux_") + type_name;
+    cl::Kernel kernel_add(gpuContext.program, add_name.c_str());
     cl::KernelFunctor add_aux(kernel_add, gpuContext.queue, cl::NullRange, cl::NDRange(rounded_size), cl::NDRange(block_size));
-    cl::Event addEvent = add_aux(dev_aux, out, input_size);
+    cl::Event addEvent = add_aux(dev_scan_aux, out, input_size);
     addEvent.wait(); 
 }
 
-
-int main()
-{
+template<typename T>
+void exec(std::vector<T>& input, const std::string& type_name) {
+    typedef std::vector<T> array;
     std::vector<cl::Platform> platforms;
     std::vector<cl::Device> devices;
     std::vector<cl::Kernel> kernels;
@@ -81,27 +86,21 @@ int main()
         // compile opencl source
         program.build(devices);
       
-        // read input file
-        std::ifstream inputFile("input.txt");
-        size_t input_size;
-        inputFile >> input_size;
+
 	  
-        array input(input_size);
+        size_t input_size = input.size();
         size_t result_size = input_size;
         array result(result_size);
-        for (size_t i = 0; i < input_size; i++) {
-            inputFile >> input[i];
-        } 
 	  
         // allocate device buffer to hold message
-        cl::Buffer dev_in(context, CL_MEM_READ_ONLY,  sizeof(element_type) * input_size);
-        cl::Buffer dev_out(context, CL_MEM_READ_WRITE, sizeof(element_type) * result_size);
+        cl::Buffer dev_in(context, CL_MEM_READ_ONLY,  sizeof(T) * input_size);
+        cl::Buffer dev_out(context, CL_MEM_READ_WRITE, sizeof(T) * result_size);
 
         // copy from cpu to gpu
-        queue.enqueueWriteBuffer(dev_in, CL_TRUE, 0, sizeof(element_type) * input_size, &input[0]);	
+        queue.enqueueWriteBuffer(dev_in, CL_TRUE, 0, sizeof(T) * input_size, &input[0]);	
         GpuContext gpuContext(context, queue, program);
-        scan(gpuContext, dev_in, dev_out, input_size);
-        queue.enqueueReadBuffer(dev_out, CL_TRUE, 0, sizeof(element_type) * result_size, &result[0]);
+        scan<T>(gpuContext, dev_in, dev_out, input_size, type_name);
+        queue.enqueueReadBuffer(dev_out, CL_TRUE, 0, sizeof(T) * result_size, &result[0]);
 		
         //print result
         std::ofstream outputFile("output.txt");
@@ -113,6 +112,35 @@ int main()
     {
         std::cout << std::endl << e.what() << " : " << e.err() << std::endl;
     }
-   
+    
+    
+}
+
+
+int main()
+{
+    // read input file
+    std::ifstream inputFile("input.txt");
+    size_t input_size;
+    inputFile >> input_size;
+    inputFile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::string data;
+    std::getline(inputFile, data);
+    bool is_float = data.find('.') != std::string::npos;
+    std::stringstream stream(data);
+    if (is_float) {
+        std::vector<float> input(input_size);
+        for (size_t i = 0; i < input_size; i++) {
+            stream >> input[i];
+        } 
+        exec<float>(input, "float");
+    } else {
+        std::vector<int> input(input_size);
+        for (size_t i = 0; i < input_size; i++) {
+            stream >> input[i];
+        } 
+        exec<int>(input, "int");
+    }
+
     return 0;
 }
---
+++ hw2/test.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import sys
+import subprocess
+
+def scan(f, state, it):
+  for x in it:
+    state = f(state, x)
+    yield state
+    
+def runProg(bin, stdin = None):
+    pipe = subprocess.Popen([bin],
+                            stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    (out, err) = pipe.communicate()
+    return out
+
+def test(n):
+    seq = [1] * n
+    with open('input.txt', 'w') as f:   
+        f.write(str(n) + "\n")
+        in_test = map(lambda x: str(x), seq)
+        f.write(' '.join(in_test))
+    runProg(sys.argv[1])
+    expect = list(scan(lambda x,y: x + y, 0, seq))
+    with open('output.txt', 'r') as f:
+        data = f.read()
+        out = map(lambda x: float(x), data.strip().split(' '))
+        test = filter(lambda (x,y): x != y, zip(out, expect))
+        if len(test) == 0:
+            print("OK : [1] * " + str(n))
+        else:
+            print("FAIL [1] * " + str(n))
+            print("get : " + str(test[0][0]) + " expect : " + str(test[0][1]))    
+        
+
+def main():
+    if len(sys.argv) < 2:
+        print("usage : ./test.py executable_name")
+        return
+    test(5)
+    n = 48576;
+    while(n <= 1048576):
+        test(n)
+        n += 100000
+        
+        
+main()
